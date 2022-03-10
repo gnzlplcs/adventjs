6990af44cf85e62a140a277e9fd11e72e3daf89b
gnzlplcs <palaciosgm@outlook.com>
2022-03-10 16:14:24 -0500

    ✅ ovejas rojas filtradas

---
+++ challenge-01/index.js
@@ -0,0 +1,13 @@
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'},
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+const isRed = el => el.color === 'rojo';
+
+let redSheep = ovejas.filter(isRed)
+console.log(redSheep)
